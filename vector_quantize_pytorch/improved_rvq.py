15e8746a70b60efbbba11fe92ec790c0e93b4b94
bhkim98 <bhkim98@dsp.yonsei.ac.kr>
2024-10-23 15:31:51 +0900

    Minor

--- vector_quantize_pytorch/improved_rvq.py
+++ vector_quantize_pytorch/improved_rvq.py
@@ -213,8 +213,6 @@ class ImprovedRVQ(nn.Module):
 
         for i, quantizer in enumerate(self.quantizers):
             if self.training is False and i >= n_active_cb:
-                if i == 0:
-                    z_q = torch.zeros_like(z).to(z.device)
                 break
 
             z_q_i, indices_i, commit_loss_i, codebook_loss_i = quantizer(residual)
@@ -230,10 +228,7 @@ class ImprovedRVQ(nn.Module):
 
             codebook_indices.append(indices_i)
 
-        if n_active_cb >= 1:
-            codes = torch.stack(codebook_indices, dim=-1)    # [B, T, Nq]
-        else:
-            codes = None
+        codes = torch.stack(codebook_indices, dim=-1)    # [B, T, Nq]
 
         return z_q, codes, commit_loss, codebook_loss # [B, D, T], [B, T, Nq], ...
 
